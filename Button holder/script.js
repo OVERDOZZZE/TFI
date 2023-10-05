6a027996ea99138d6a8af57ac4391a6698a3d7df
OVERDOZZZE <saparbekov113@gmail.com>
2023-10-06 01:57:19 +0600

    First commit

---
+++ Button holder/script.js
@@ -0,0 +1,36 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+
+item.addEventListener("dragstart", dragstart);
+item.addEventListener("dragend", dragend);
+
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener("dragover", dragover);
+    placeholder.addEventListener("drop", drop);
+}
+
+function dragstart(event) {
+    event.target.classList.add("hold");
+    setTimeout(() => event.target.classList.add("hide"), 0);
+
+    console.log(event.target);
+}
+
+function dragend(event) {
+    event.target.className = "item";
+    console.log(event.target);
+}
+
+function dragover(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.target.append(item);
+}
+
+
+
+
+
